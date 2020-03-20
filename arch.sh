fe869a2af79020eab4436791400bb937261949f6
Maico Tedesco <maicotm86@gmail.com>
2020-03-20 17:22:21 -0300

    Add new apps

--- arch.sh
+++ arch.sh
@@ -26,6 +26,13 @@
 #passwd maico
 #sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 # exit
+
+sudo pacman -S xorg-server xorg-xwininfo xorg-xinit xorg-xprop xorg-xdpyinfo xorg-xbacklight ttf-inconsolata ttf-linux-libertine arandr bc calcurse xcompmgr dosfstools libnotify dunst exfat-utils sxiv xwallpaper ffmpeg gnome-keyring neovim mpd mpc mpv ncmpcpp noto-fonts-emoji ntfs-3g alsa-utils maim socat tmux unclutter unrar unzip lynx xcape xclip xdotool youtube-dl zathura zathura-pdf-mupdf poppler mediainfo atool fzf highlight zsh-syntax-highlighting slock
+
+
+yay -S lf-git gtk-theme-arc-gruvbox-git brave-bin sc-im task-spooler simple-mtpfs-git mutt-wizard-git 
+
+
 sudo pacman -S archlinux-keyring xorg-server xorg-apps xorg-xinit xf86-input-libinput xf86-input-mouse xf86-input-synaptics xf86-input-keyboard xf86-video-intel pulseaudio pulseaudio-alsa i3-gaps i3lock i3blocks dmenu ntfs-3g p7zip unzip unrar xfce4-settings networkmanager network-manager-applet compton libreoffice-still-pt-br imagemagic volumeicon shellcheck feh nemo nemo-fileroller nemo-preview xfce4-appfinder xfce4-power-manager xfce4-notifyd vulkan-intel xfwm4 xfwm4-themes terminator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings python-pip qbittorrent simplescreenrecorder xcursor-simpleandsoft adobe-source-sans-pro-fonts awesome-terminal-fonts noto-fonts ttf-bitstream-vera ttf-dejavu ttf-droid ttf-hack ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family arc-icon-theme adapta-gtk-theme arc-gtk-theme dconf-editor htop tree youtube-dl zsh zsh-completions zsh-syntax-highlighting --noconfirm 
 
 localectl set-keymap br-abnt2
