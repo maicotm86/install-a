145fa4953cfefb0cb579f6b457d885291bd6dfdf
Maico Tedesco <maicotm86@gmail.com>
2020-04-05 16:55:34 -0300

    man-db

--- arch.sh
+++ arch.sh
@@ -4,7 +4,7 @@
 #dd if=/dev/zero of=/dev/sd?? bs=512 count=1 conv=notrunc status="progress"
 
 # pacstrap
-#pacstrap -i /mnt base base-devel linux-lts linux-lts-headers linux-firmware bash-completion vim git curl wget
+#pacstrap -i /mnt base base-devel linux-lts linux-lts-headers linux-firmware bash-completion vim git curl wget man-db
 #genfstab -U /mnt >> /mnt/etc/fstab
 
 #loadkeys br-abnt2
@@ -32,13 +32,13 @@
 #sudo pacman -S xorg-server xorg-xwininfo xorg-xinit xorg-xprop xorg-xdpyinfo xorg-xbacklight ttf-inconsolata ttf-linux-libertine arandr bc calcurse xcompmgr dosfstools libnotify dunst exfat-utils sxiv xwallpaper ffmpeg gnome-keyring neovim mpd mpc mpv ncmpcpp noto-fonts-emoji ntfs-3g alsa-utils maim socat tmux unclutter unrar unzip lynx xcape xclip xdotool youtube-dl zathura zathura-pdf-mupdf poppler mediainfo atool fzf highlight zsh-syntax-highlighting slock xf86-input-libinput network-manager-applet volumeicon xf86-video-intel xf86-input-libinput python-pip --noconfirm
 
 
-#yay -S gtk-theme-arc-gruvbox-git brave-bin sc-im task-spooler simple-mtpfs-git mutt-wizard-git 
+#yay -S gtk-theme-arc-gruvbox-git brave-bin sc-im task-spooler simple-mtpfs-git mutt-wizard-git
 
 
-#yay -S lf-git gtk-theme-arc-gruvbox-git brave-bin sc-im task-spooler simple-mtpfs-git mutt-wizard-git 
+#yay -S lf-git gtk-theme-arc-gruvbox-git brave-bin sc-im task-spooler simple-mtpfs-git mutt-wizard-git
 
 
-#sudo pacman -S archlinux-keyring xorg-server xorg-apps xorg-xinit xf86-input-libinput xf86-input-mouse xf86-input-synaptics xf86-input-keyboard xf86-video-intel pulseaudio pulseaudio-alsa i3-gaps i3lock i3blocks dmenu ntfs-3g p7zip unzip unrar xfce4-settings networkmanager network-manager-applet compton libreoffice-still-pt-br imagemagic volumeicon shellcheck feh nemo nemo-fileroller nemo-preview xfce4-appfinder xfce4-power-manager xfce4-notifyd vulkan-intel xfwm4 xfwm4-themes terminator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings python-pip qbittorrent simplescreenrecorder xcursor-simpleandsoft adobe-source-sans-pro-fonts awesome-terminal-fonts noto-fonts ttf-bitstream-vera ttf-dejavu ttf-droid ttf-hack ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family arc-icon-theme adapta-gtk-theme arc-gtk-theme dconf-editor htop tree youtube-dl zsh zsh-completions zsh-syntax-highlighting --noconfirm 
+#sudo pacman -S archlinux-keyring xorg-server xorg-apps xorg-xinit xf86-input-libinput xf86-input-mouse xf86-input-synaptics xf86-input-keyboard xf86-video-intel pulseaudio pulseaudio-alsa i3-gaps i3lock i3blocks dmenu ntfs-3g p7zip unzip unrar xfce4-settings networkmanager network-manager-applet compton libreoffice-still-pt-br imagemagic volumeicon shellcheck feh nemo nemo-fileroller nemo-preview xfce4-appfinder xfce4-power-manager xfce4-notifyd vulkan-intel xfwm4 xfwm4-themes terminator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings python-pip qbittorrent simplescreenrecorder xcursor-simpleandsoft adobe-source-sans-pro-fonts awesome-terminal-fonts noto-fonts ttf-bitstream-vera ttf-dejavu ttf-droid ttf-hack ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family arc-icon-theme adapta-gtk-theme arc-gtk-theme dconf-editor htop tree youtube-dl zsh zsh-completions zsh-syntax-highlighting --noconfirm
 
 #localectl set-keymap br-abnt2
 #localectl set-x11-keymap br
